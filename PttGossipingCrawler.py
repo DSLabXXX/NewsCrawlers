2bdf65e572a1cffdfc4d4644d1003c6b2ce78682
SWhite4han <elic7772425@gmail.com>
2017-06-13 17:00:30 +0800

    Add auto crawl func. Fix bug with parse date and img url.

--- PttGossipingCrawler.py
+++ PttGossipingCrawler.py
@@ -14,15 +14,15 @@ from bs4.element import NavigableString
 
 class PttCrawler(object):
 
-    root = "https://www.ptt.cc/bbs/"
-    main = "https://www.ptt.cc"
+    root = 'https://www.ptt.cc/bbs/'
+    main = 'https://www.ptt.cc'
     gossip_data = {
-        "from": "bbs/Gossiping/index.html",
-        "yes": "yes"
+        'from': 'bbs/Gossiping/index.html',
+        'yes': 'yes'
     }
     board = ''
 
-    # file path. Will be instead of config file
+    # File path. Will be removed in later version and using config file to instead of it.
     file_root = '/data1/'
 
     moon_trans = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04',
@@ -32,18 +32,64 @@ class PttCrawler(object):
     def __init__(self):
         self.session = requests.session()
         requests.packages.urllib3.disable_warnings()
-        self.session.post("https://www.ptt.cc/ask/over18",
+        self.session.post('https://www.ptt.cc/ask/over18',
                           verify=False,
                           data=self.gossip_data)
+
+    def get_articles(self, page, date, json_file):
+        res = self.session.get(page, verify=False)
+        soup = BeautifulSoup(res.text, 'lxml')
+
+        # Load got files list
+        with open(json_file, 'r') as rf:
+            got_articles = json.load(rf)
+
+        check_set = set()
+        if len(got_articles) > 0:
+            for i in got_articles:
+                check_set.add(i['href'])
+
+        articles = []  # 儲存取得的文章資料
+        divs = soup.find_all('div', 'r-ent')
+        for d in divs:
+            # Check 有超連結，表示文章存在，未被刪除 and 發文日期正確
+            if d.find('a') and d.find('div', 'date').string.strip() == date:
+                href = self.main + d.find('a')['href']
+                # Check link is not redundant.
+                if href not in check_set:
+                    # 取得推文數
+                    push_count = 0
+                    if d.find('div', 'nrec').string:
+                        try:
+                            push_count = int(d.find('div', 'nrec').string)  # 轉換字串為數字
+                        except ValueError:  # 若轉換失敗，不做任何事，push_count 保持為 0
+                            pass
+                    # 取得文章標題
+                    title = d.find('a').string
+                    # Add to got articles list
+                    articles.append(href)
+                    got_articles.append({
+                        'title': title,
+                        'href': href,
+                        'push_count': push_count
+                    })
+
+        with open(json_file, 'w') as op:
+            json.dump(got_articles, op, indent=4, ensure_ascii=False)
+
+        # 取得上一頁的連結
+        paging_div = soup.find('div', 'btn-group btn-group-paging')
+        prev_url = self.main + paging_div.find_all('a')[1]['href']
+        return articles, prev_url
 
     def articles(self, page):
 
         res = self.session.get(page, verify=False)
-        soup = BeautifulSoup(res.text, "lxml")
+        soup = BeautifulSoup(res.text, 'lxml')
 
-        for article in soup.select(".r-ent"):
+        for article in soup.select('.r-ent'):
             try:
-                yield self.main + article.select(".title")[0].select("a")[0].get("href")
+                yield self.main + article.select('.title')[0].select('a')[0].get('href')
             except Exception as e:
                 # (本文已被刪除)
                 # logging.exception(e)
@@ -51,13 +97,13 @@ class PttCrawler(object):
 
     def pages(self, board=None, index_range=None):
 
-        target_page = self.root + board + "/index"
+        target_page = self.root + board + '/index'
 
         if index_range is None:
-            yield target_page + ".html"
+            yield target_page + '.html'
         else:
             for index in index_range:
-                yield target_page + str(index) + ".html"
+                yield target_page + str(index) + '.html'
 
     def parse_date(self, date_data):
         try:
@@ -67,11 +113,11 @@ class PttCrawler(object):
 
             date = date_data[-1] + self.moon_trans[date_data[1]] + date_data[2]
             # split 16:24:41
-            for i in date_data[3].split(":"):
+            for i in date_data[3].split(':'):
                 date += i
         except Exception as e:
             print(e)
-            print(u"在分析 date 時出現錯誤")
+            print(u'在分析 date 時出現錯誤')
         return date
 
     def parse_url(self, links):
@@ -79,50 +125,49 @@ class PttCrawler(object):
             img_urls = []
             link_urls = []
             for link in links:
-                if re.match(r'^https?://(i.)?(m.)?imgur.com', link['href']):
+                if re.match(r'\S+?\.(?:jpg|jpeg|gif|png)', link['href']) or re.match(r'^https?://(i.)?(m.)?imgur.com', link['href']):
                     img_urls.append(link['href'])
                 else:
                     link_urls.append(link['href'])
         except Exception as e:
             print(e)
-            print(u"在分析 url 時出現錯誤")
+            print(u'在分析 url 時出現錯誤')
         return img_urls, link_urls
 
     def parse_article(self, url):
-
         raw = self.session.get(url, verify=False)
-        soup = BeautifulSoup(raw.text, "lxml")
+        soup = BeautifulSoup(raw.text, 'lxml')
 
         try:
             article = dict()
 
-            article["URL"] = url
+            article['URL'] = url
 
             # 取得文章作者與文章標題
-            article["Author"] = soup.select(".article-meta-value")[0].contents[0].split(" ")[0]
-            article["Title"] = soup.select(".article-meta-value")[2].contents[0]
+            article['Author'] = soup.select('.article-meta-value')[0].contents[0].split(' ')[0]
+            article['Title'] = soup.select('.article-meta-value')[2].contents[0]
 
             # 取得文章 Date
-            article["Date"] = self.parse_date(soup.select(".article-meta-value")[3].contents[0].split())
+            article['Date'] = self.parse_date(soup.select('.article-meta-value')[-1].contents[0].split())
 
             # 取得內文
-            content = ""
+            content = ''
             links = list()
-            for tag in soup.select("#main-content")[0]:
+            for tag in soup.select('#main-content')[0]:
                 if type(tag) is NavigableString and tag != '\n':
                     content += tag
                 elif tag.name == 'a':
                     links.append(tag)
-            article["Content"] = content
+            article['Content'] = content
 
             # 取得 Img & Link url
-            article["ImgUrl"], article["LinkUrl"] = self.parse_url(links)
+            article['ImgUrl'], article['LinkUrl'] = self.parse_url(links)
 
             # Get Author IP & Article URL
-            for tag_f2 in soup.select(".f2"):
-                sp = tag_f2.text.split(" ")
+            for tag_f2 in soup.select('.f2'):
+                sp = tag_f2.text.split(' ')
                 if len(sp) > 1 and sp[1] == '發信站:':
-                    article["AuthorIp"] = sp[-1].split('\n')[0]
+                    article['AuthorIp'] = sp[-1].split('\n')[0]
 
             # 處理回文資訊
             upvote = 0
@@ -130,48 +175,48 @@ class PttCrawler(object):
             novote = 0
             response_list = []
 
-            for response_struct in soup.select(".push"):
+            for response_struct in soup.select('.push'):
 
                 # 跳脫「檔案過大！部分文章無法顯示」的 push class
-                if "warning-box" not in response_struct['class']:
+                if 'warning-box' not in response_struct['class']:
 
                     response_dic = dict()
-                    response_dic["Content"] = response_struct.select(".push-content")[0].contents[0][2:]
-                    response_dic["Vote"] = response_struct.select(".push-tag")[0].contents[0][0]
-                    response_dic["User"] = response_struct.select(".push-userid")[0].contents[0]
-                    response_dic["Date"] = response_struct.select(".push-ipdatetime")[0].contents[0].split("\n")[0]
+                    response_dic['Content'] = response_struct.select('.push-content')[0].contents[0][2:]
+                    response_dic['Vote'] = response_struct.select('.push-tag')[0].contents[0][0]
+                    response_dic['User'] = response_struct.select('.push-userid')[0].contents[0]
+                    response_dic['Date'] = response_struct.select('.push-ipdatetime')[0].contents[0].split('\n')[0]
                     response_list.append(response_dic)
 
-                    if response_dic["Vote"] == u"推":
+                    if response_dic['Vote'] == u'推':
                         upvote += 1
-                    elif response_dic["Vote"] == u"噓":
+                    elif response_dic['Vote'] == u'噓':
                         downvote += 1
                     else:
                         novote += 1
 
-            article["Push"] = response_list
-            article["UpVote"] = upvote
-            article["DownVote"] = downvote
-            article["NoVote"] = novote
+            article['Push'] = response_list
+            article['UpVote'] = upvote
+            article['DownVote'] = downvote
+            article['NoVote'] = novote
 
             # Other keys
             # -----------------------------------------------------------------------
             # for NLP
-            article["KeyWord"] = ''
-            article["SplitText"] = ''
+            article['KeyWord'] = ''
+            article['SplitText'] = ''
             # for NER
-            article["Org"] = ''
-            article["People"] = ''
-            article["Location"] = ''
+            article['Org'] = ''
+            article['People'] = ''
+            article['Location'] = ''
             # for Analysis
-            article["Event"] = ''
-            article["HDFSurl"] = ''
+            article['Event'] = ''
+            article['HDFSurl'] = ''
 
-            article["Source"] = 'Ptt' + self.board
+            article['Source'] = 'Ptt' + self.board
 
         except Exception as e:
             print(e)
-            print(u"在分析 %s 時出現錯誤" % url)
+            print(u'在分析 %s 時出現錯誤' % url)
 
         return article
 
@@ -182,14 +227,14 @@ class PttCrawler(object):
             if not os.path.isdir(file_path):
                 os.makedirs(file_path)
 
-            with open(file_path + filename + ".json", 'w') as op:
+            with open(file_path + filename + '.json', 'w') as op:
                 # op.write(json.dumps(data, indent=4, ensure_ascii=False).encode('utf-8'))
                 json.dump(data, op, indent=4, ensure_ascii=False)
         except Exception as e:
             print(e)
-            print(u"在 Check Folder or Save File 時出現錯誤")
+            print(u'在 Check Folder or Save File 時出現錯誤')
 
-    def crawl(self, board="Gossiping", start=1, end=2, sleep_time=0.5):
+    def crawl(self, board='Gossiping', start=1, end=2, sleep_time=0.5):
         self.board = board
         crawl_range = range(start, end)
         # for Test
@@ -201,14 +246,51 @@ class PttCrawler(object):
                 self.save_article(board, '%s_' % art['Date'] + str(art['Title']) + '_%s' % art['Author'], art)
                 time.sleep(sleep_time)
 
-            print(u"已經完成 %s 頁面第 %d 頁的爬取" % (board, start))
+            print(u'已經完成 %s 頁面第 %d 頁的爬取' % (board, start))
             start += 1
+
+    def auto_crawl(self, board='Gossiping', sleep_time=0.5):
+        home_page = self.root + board + '/index.html'
+        today_articles = list()
+        today = time.strftime('%m/%d').lstrip('0')
+        today_path = time.strftime('%Y%m%d')
+        file_path = self.file_root + 'Ptt/' + board + '/' + today_path + '/'
+
+        # check folder
+        if not os.path.isdir(file_path):
+            os.makedirs(file_path)
+
+        # check crawled list
+        json_today = file_path + today_path + '.json'
+        if not os.path.isfile(json_today):
+            with open(json_today, 'w') as wf:
+                json.dump(today_articles, wf)
+
+        articles, pre_link = self.get_articles(home_page, today, json_today)
+        while articles:
+            today_articles += articles
+            articles, pre_link = self.get_articles(pre_link, today, json_today)
+        print(len(today_articles))
+
+        for art_link in today_articles:
+            art = self.parse_article(art_link)
+            try:
+                file_name = '%s_' % art['Date'] + str(art['Title']) + '_%s' % art['Author']
+            except Exception as e:
+                print(e)
+                file_name = 'UnkownFileName_%d' % time.time()
+            self.save_article(board, file_name, art)
+            time.sleep(sleep_time)
 
 
 def main():
 
     crawler = PttCrawler()
-    crawler.crawl(board="Gossiping", start=22578, end=22579)
+    # crawler.crawl(board='Gossiping', start=22500, end=22501)
+    crawler.auto_crawl(board='Gossiping')
+    # for test
+    # art = crawler.parse_article('https://www.ptt.cc/bbs/Gossiping/M.1497312830.A.755.html')
+    # art = crawler.parse_article('https://www.ptt.cc/bbs/Gossiping/M.1497338721.A.E5B.html')
 
 if __name__ == '__main__':
     main()
